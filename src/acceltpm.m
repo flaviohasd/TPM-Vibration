c1bbae7a81367cd04e7a38427d3294ef693f1370
Flávio Dias <44821460+flaviohasd@users.noreply.github.com>
2023-04-09 15:58:56 -0400

    Delete src directory

--- src/acceltpm.m
+++
@@ -1,4 +0,0 @@
-function a = acceltpm(states,K,C,F,me,d1,d2,K1,K2)
-    a(1) = states(2);
-    a(2) = (1/me)*(-K*states(1) -C*states(2) + F + K1*d1 + K2*d2);
-end
--- src/operation.m
+++
@@ -1,8 +0,0 @@
-function [w,w1,w2,T,F,freq,ciclo] = operation(Pot,W,db,dp1,dp2)
-    w = W*(2*pi/60); % Pinion rotation (rad/s)
-    w1 = w; w2 = -w1*dp1/dp2; % (rad/s)
-    freq = w/(2*pi); % Frequency of pinion rotation (Hz)
-    ciclo = 2*pi/w; % Period of revolution of the pinion (s) (used as the minimum analysis time)
-    T = Pot*(60/(2*pi))/W; % Torque acting in the line of action (Nm) (pinion)
-    F = T/(db/2000); % Force acting in the line of action (N)
-end
