95e948b9a167e040dccb93b6f38c6d1c5bf72b0e
Flávio Dias <44821460+flaviohasd@users.noreply.github.com>
2023-04-08 20:11:34 -0400

    Delete src directory

--- src/dinamica.m
+++
@@ -1,9 +0,0 @@
-function dxdt = dinamica(x,t,K,C,F,me) % PADRAO
-
-%% MATRIZ DO SISTEMA E ENTRADA
-A = [0 1; -K(t)/me -C(t)/me];
-B = [0; 1/me];
-
-%% SISTEMA
-    dxdt = A*x + B*F;
-end
--- src/dinamicatpm.m
+++
@@ -1,9 +0,0 @@
-function dxdt = dinamicatpm(x,t,K,C,F,me,d1,d2,K1,K2) % PADRAO
-
-%% MATRIZ DO SISTEMA E ENTRADA
-A = [0 1; -K(t)/me -C(t)/me];
-B = [0; 1/me];
-
-%% SISTEMA
-    dxdt = A*x + B*(F + K1(t)*d1(t) + K2(t)*d2(t));
-end
